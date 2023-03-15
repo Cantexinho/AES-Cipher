36eb1fe2d6cefb3aa4f25ba75f4152f4bed31f89
Cantexinho <cantexinho@gmail.com>
2023-03-15 16:22:18 +0200

    Added the initial frame

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Choose Cypher/Decypher:")
+
+chosen_type = input()
+
+print("Type in your text:")
+
+text_to_process = input()
+
+print("Type in the secret key:")
+
+secret_key = input()
+
+if chosen_type.lower() == "cypher":
+    print()
+elif chosen_type.lower() == "decypher":
+    print()
