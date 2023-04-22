5bf6877db0151f27c43c8b1c8a73a84846907009
Cantexinho <cantexinho@gmail.com>
2023-04-22 20:38:42 +0300

    Added functions to put or read text from file

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from cbc_cipher import CbcCipher as Cbc
 from cfb_cipher import CfbCipher as Cfb
 from ofb_cipher import OfbCipher as Ofb
 
+# Using while(true) statements is bad, however bad input can be worse
+
 def select_cipher_decypher() -> str:
     while True:
         chosen_type = input("Choose Cipher/Decipher: ")
@@ -33,24 +35,62 @@ def select_cipher_mode():
             if chosen_mode.lower() == "ofb":
                 return Ofb()
 
-def cipher_text(cipher_mode: object) -> None:
+def should_read_from_file() -> bool:
+    while True:
+        answer = input("Do you want to read the text from file? (Yes/No) ")
+        if answer.lower() not in ["yes", "no"]:
+            print(f"Error: Your must say Yes or No!")
+        else:
+            if answer.lower() == "yes":
+                return True
+            if answer.lower() == "no":
+                return False
+
+def read_from_file(file_name: str) -> str:
+    file_name = file_name.strip()
+    with open(f"{file_name}.txt", "r") as file:
+        content = file.read()
+        return bytes.fromhex(content.strip())
+
+def cipher_text(cipher_mode: object) -> str:
     text_to_process = select_cipher_text()
     hex_cipher_text = cipher_mode.cipher_text(text_to_process).hex()
     print(f"Ciphered text: {hex_cipher_text}")
+    return hex_cipher_text
 
-def decipher_text(cipher_mode: object) -> None:
-    text_to_process = select_decipher_text()
+def decipher_text(cipher_mode: object) -> str:
+    if not should_read_from_file():
+        text_to_process = select_decipher_text()
+    else:
+        file_name = input("Type in the file name: ")
+        text_to_process = read_from_file(file_name)
     plain_deciphered_text = cipher_mode.decipher_text(text_to_process).decode('utf-8')
     print(f"Deciphered text: {plain_deciphered_text}")
+    return plain_deciphered_text
+
+def write_to_file(text: str) -> None:
+    while True:
+        answer = input("Do you want to put this text to file? (Yes/No) ")
+        if answer.lower() not in ["yes", "no"]:
+            print(f"Error: Your must say Yes or No!")
+        else:
+            if answer.lower() == "yes":
+                with open('your_cipher.txt', 'w+') as file:
+                    file.write(text)
+                    return
+            if answer.lower() == "no":
+                return         
 
 def main():
     chosen_type = select_cipher_decypher()
     cipher_mode = select_cipher_mode()
 
     if chosen_type.lower() == "cipher":
-        cipher_text(cipher_mode)
+        text = cipher_text(cipher_mode)
     elif chosen_type.lower() == "decipher":
-        decipher_text(cipher_mode)
+        text = decipher_text(cipher_mode)
+
+    write_to_file(text)
 
 
 
